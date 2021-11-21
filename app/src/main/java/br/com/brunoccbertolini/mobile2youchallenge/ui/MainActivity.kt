988f180437f2d76e3ab6ece1150c13c0ecf254dd
Bruno Bertolini <bruno.bertolini@hotmail.com>
2021-11-20 22:09:04 -0300

    chore: setup navigation component
    created ->res/navigation/nav_graph
    
    MainActivity as navigation container

---
+++ app/src/main/java/br/com/brunoccbertolini/mobile2youchallenge/ui/MainActivity.kt
@@ -0,0 +1,44 @@
+package br.com.brunoccbertolini.mobile2youchallenge.ui
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import br.com.brunoccbertolini.mobile2youchallenge.R
+import br.com.brunoccbertolini.mobile2youchallenge.databinding.ActivityMainBinding
+import br.com.brunoccbertolini.myapplication.viewmodel.MoviesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val moviesViewModel: MoviesViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(1500) //Be removed!
+        setTheme(R.style.Theme_Mobile2YouChallenge)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        moviesViewModel.getMoviesNowPlaying()
+
+        val newNavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.drinksNavHostFragment) as NavHostFragment
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
